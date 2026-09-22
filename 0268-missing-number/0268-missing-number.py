2a680a32ce717a59a89f6c9d8b68b312bd5a6429
luckydixit0464 <luckydixit0464@gmail.com>
2026-09-22 20:19:28 +0530

    Time: 3 ms (60.69%), Space: 20.3 MB (76.23%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,10 +1,24 @@
 class Solution:
     def missingNumber(self, nums: list[int]) -> int:
-        # for i in range(len(nums)):
+        fount=False
+        # for i in range(len(nums)+1):
+        #     fount=False
         #     for j in range(len(nums)):
         #         if nums[j]==i:
+        #             found=True
         #             break
-        # return i
-        sum1=sum(nums)
-        sum2=len(nums)*(len(nums)+1)/2
-        return int(sum2-sum1)
+        #     if not found:
+        #        return i
+
+        # sum1=sum(nums)
+        # sum2=len(nums)*(len(nums)+1)/2
+        # return int(sum2-sum1)
+    
+        expected=0
+        expected1=0
+        for i in range(len(nums)):
+            expected=expected^nums[i]
+        for i in range(len(nums)+1):
+            expected1=expected1^i
+        return expected^expected1
+
